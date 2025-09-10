859222abc711af0e05d2283d84b6db9de7d2ac8a
Shivam kumar Jha <shivamjlr255@gmail.com>
2025-09-10 15:05:24 +0530

    Updated backend .env file for frontend URLs

--- frontend/src/pages/apiConfig.js
+++ frontend/src/pages/apiConfig.js
@@ -1,12 +1,14 @@
 let API_BASE_URL;
 
-
-if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") {
-
+if (
+  window.location.hostname === "localhost" ||
+  window.location.hostname === "127.0.0.1"
+) {
+  
   API_BASE_URL = "http://localhost:5000";
 } else {
 
-  API_BASE_URL = `http://${window.location.hostname}:5000`;
+  API_BASE_URL = "https://back2campus.onrender.com";
 }
 
 export default API_BASE_URL;
